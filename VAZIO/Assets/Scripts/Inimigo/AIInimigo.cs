e8d8c71f0eb24f4678ec9252787a982e7d946b65
Ardeo <josefigueiredo2001@hotmail.com>
2018-02-24 17:10:10 +0000

    melhoramentos do ai

--- VAZIO/Assets/Scripts/Inimigo/AIInimigo.cs
+++ VAZIO/Assets/Scripts/Inimigo/AIInimigo.cs
@@ -5,19 +5,67 @@ using UnityEngine;
 public class AIInimigo : MonoBehaviour {
 
     public Transform player;
+
+	//atributos predefinidos
     private int rotSpeed = 180;
     private int speed = 4;
-	
+
+	//objetos referentes ao campo de deteção
+	public float defaultRadius = 5f;
+	public float currentRadius;
+
+	//objetos referentes às armas
+	public GameObject weapon01, weapon02;
+
+	//variáveis referentes à linha de tiro
+	public Transform lineBegin, lineEnd;
+	private bool targetOnSight = false;
+
+	void Start() {
+		weapon01.SetActive (false);
+		weapon02.SetActive (false);
+	}
+
 	void Update () {
-
-        //encontrar jogador por tag "player"
-        findPlayer();
-        //mover a nave
-        shipMove();
-
+		alterRadius ();
+		if (lookForTarget()) {
+			//encontrar jogador por tag "player"
+			findPlayer ();
+			//mover a nave
+			shipMove ();
+			//verificar linha de tiro e disparar
+			checkShootingLine ();
+		}
     }
 
-    void shipMove()
+	//alterar o raio do campo de visão se o jogador se encontra ou não dentro dele
+	private void alterRadius() {
+		if (lookForTarget()) {currentRadius = defaultRadius * 3;}
+		else if (!lookForTarget()) {currentRadius = defaultRadius;}
+	}
+
+	//verificar se o jogador se encontra no campo de visão do inimigo
+	public bool lookForTarget(){
+		bool targetAquired = Physics2D.CircleCast (this.transform.position, currentRadius, 
+				new Vector2 (this.transform.position.x, this.transform.position.y), 0, 1 << LayerMask.NameToLayer ("Player"));
+		if(targetOnSight = false) {targetAquired = false;}
+		return targetAquired;
+	}
+
+	//verificar se o jogador se encontra na linha de tiro do inimigo e disparar
+	private void checkShootingLine() {
+		targetOnSight = Physics2D.Linecast (lineBegin.position, lineEnd.position, 1 << LayerMask.NameToLayer ("Player"));
+		if (targetOnSight) {
+			weapon01.SetActive (true);
+			weapon02.SetActive (true);
+		} else {
+			weapon01.SetActive (false);
+			weapon02.SetActive (false);
+		}
+	}
+
+	//mover o inimigo
+    private void shipMove()
     {
         //girar para a direção do jogador
         Vector3 dir = player.position - transform.position;
@@ -26,14 +74,18 @@ public class AIInimigo : MonoBehaviour {
         Quaternion desiredRotation = Quaternion.Euler(0, 0, zAngle);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, desiredRotation, rotSpeed * Time.deltaTime);
 
-        //deslocamento da nave
-        Vector3 pos = transform.position;
-        Vector3 velocity = new Vector3(0, speed * Time.deltaTime, 0);
-        pos += transform.rotation * velocity;
-        transform.position = pos;
+        //deslocamento da nave se o inimigo se encontra a uma distancia maior do que x do jogador
+		if (Vector2.Distance (this.transform.position, player.position) > 3) {
+			Vector3 pos = transform.position;
+			Vector3 velocity = new Vector3 (0, speed * Time.deltaTime, 0);
+			pos += transform.rotation * velocity;
+			transform.position = pos;
+		} else
+			return;
     }
 
-    void findPlayer()
+	//encontrar o jogador pela tag
+    private void findPlayer()
     {
         if (player == null)
         {
@@ -46,6 +98,8 @@ public class AIInimigo : MonoBehaviour {
         if (player == null) return;
     }
 
+
+	//getters e setters
     public void setRotSpeed(int _rotSpeed)
     {
         rotSpeed = _rotSpeed;
